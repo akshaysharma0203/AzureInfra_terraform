cfa49d8b08a3b8c8d9c700d78989f9cf9c53af6f
akshaysharma0203 <55734710+akshaysharma0203@users.noreply.github.com>
2020-07-21 16:51:43 +0530

    first commit

---
+++ modules/resources/vnet/variables.tf
@@ -0,0 +1,12 @@
+variable "vnet_count" {}
+variable "regname" {}
+variable "subnet_count" {}
+variable "environment" {}
+variable "vnet_name" {}
+variable "vnet_location" {}
+variable "vnet_address_space" {}
+variable "subnet_address_space" {}
+variable "system_networks" {}
+variable "system_subnets" {}
+#variable "subnet_key" { type = list}
+#variable "listofsubnets" {type = list}
